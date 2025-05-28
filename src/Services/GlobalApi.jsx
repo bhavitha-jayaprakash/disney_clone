5ff0be40a2e6108c00ba6eff07e3ad31c9c0743f
bhavitha-jayaprakash <144882587+bhavitha-jayaprakash@users.noreply.github.com>
2025-05-28 19:22:25 +0530

    Update GlobalApi.jsx

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -5,7 +5,7 @@ const api_key=''
 
 const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key=';
 
-//https://api.themoviedb.org/3/trending/all/day?api_key=b860ca2d33c718271a8fecef78256e49
+
 const getTrendingVideos=axios.get(movieBaseUrl+
     "/trending/all/day?api_key="+api_key);
     const getMovieByGenreId=(id)=>
